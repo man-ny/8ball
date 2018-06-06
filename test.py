74ef0c3618f2f16a5ee2d3dec174f51f486d67f2
Manuel Robles <mrobles@Credibility.dandb.com>
2018-06-06 13:55:55 -0700

    this is my first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import sys
+import random
+
+from setuptools.command.easy_install import sys_executable
+
+ans = True
+
+while ans:
+
+    question = input("ask xtoes guru a question ")
+
+    answers = random.randint(1, 4)
+    if question == "":
+        print("you have to ask wuestion")
+        sys.exit()
+
+
+    elif answers == 1:
+        print("Yes, its correct")
+
+    elif answers == 2:
+        print("negative")
+
+    elif answers == 3:
+        print("possibly")
+
+    elif answers == 4:
+        print("Super wrong")
+
+    break
+
